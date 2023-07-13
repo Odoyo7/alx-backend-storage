cf88b9410b0c9641280745f5ed6a01863f03e83e
Odoyo7 <ody.daniels7@gmail.com>
2023-07-13 13:54:59 -0700

    kaput

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,4 @@
--- Script that creates a trigger that resets the attribute
--- valid_email only when the email has been changed.
-DELIMITER $$ ;
-CREATE TRIGGER validate BEFORE UPDATE ON users
-FOR EACH ROW
-BEGIN
-	IF NEW.email != OLD.email THEN
-		SET NEW.valid_email = 0;
-	END IF;
-END;$$
-delimiter ;
+-- Script that creates a trigger that decreases the
+-- quantity of an item after adding a new order.
+CREATE TRIGGER decrease_q AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;;
