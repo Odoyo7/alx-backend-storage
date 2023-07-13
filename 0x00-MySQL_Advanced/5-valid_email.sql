a0683f6dae4398d939ce405818b6459ae43e51cc
Odoyo7 <ody.daniels7@gmail.com>
2023-07-13 13:56:48 -0700

    kaput

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,16 +1,11 @@
--- Script that creates a stored procedure AddBonus
--- that adds a new correction for a student.
+-- Script that creates a trigger that resets the attribute
+-- valid_email only when the email has been changed.
 DELIMITER $$ ;
-CREATE PROCEDURE AddBonus(
-	IN user_id INTEGER,
-	IN project_name VARCHAR(255),
-	IN score INTEGER
-)
+CREATE TRIGGER validate BEFORE UPDATE ON users
+FOR EACH ROW
 BEGIN
-	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
-		INSERT INTO projects (name) VALUES (project_name);
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
 	END IF;
-	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
 END;$$
-DELIMITER ;
+delimiter ;
